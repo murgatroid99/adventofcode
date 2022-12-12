b3347dfd7ae9519036556de77997c61b24d2cf6d
Michael Lumish <michael.lumish@gmail.com>
2022-12-12 11:13:35 -0500

    Day 10: fixed off-by-1 error, reformatted

--- 2022/day10.js
+++ 2022/day10.js
@@ -4,63 +4,64 @@ function part1(input) {
   let signalStrengthSum = 0;
   const cycleTargets = new Set([20, 60, 100, 140, 180, 220]);
   function getSignalStrength() {
-      return cycle * x;
+    return cycle * x;
   }
   for (const line of input.split('\n')) {
-      const addMatch = /addx (-?\d+)/.exec(line);
-      if (line === 'noop') {
-          cycle += 1;
-          if (cycleTargets.has(cycle)) {
-              console.log(cycle, x, getSignalStrength());
-              signalStrengthSum += getSignalStrength();
-          }
-      } else if (addMatch) {
-          cycle += 1;
-          if (cycleTargets.has(cycle)) {
-              console.log(cycle, x, getSignalStrength());
-              signalStrengthSum += getSignalStrength();
-          }
-          cycle += 1;
-          if (cycleTargets.has(cycle)) {
-              console.log(cycle, x, getSignalStrength());
-              signalStrengthSum += getSignalStrength();
-          }
-          x += +addMatch[1]
+    const addMatch = /addx (-?\d+)/.exec(line);
+    if (line === 'noop') {
+      cycle += 1;
+      if (cycleTargets.has(cycle)) {
+        console.log(cycle, x, getSignalStrength());
+        signalStrengthSum += getSignalStrength();
       }
+    } else if (addMatch) {
+      cycle += 1;
+      if (cycleTargets.has(cycle)) {
+        console.log(cycle, x, getSignalStrength());
+        signalStrengthSum += getSignalStrength();
+      }
+      cycle += 1;
+      if (cycleTargets.has(cycle)) {
+        console.log(cycle, x, getSignalStrength());
+        signalStrengthSum += getSignalStrength();
+      }
+      x += +addMatch[1]
+    }
   }
   return signalStrengthSum;
 }
 
 function part2(input) {
   let x = 1;
-  let cycle = 0;
+  let pixel = 0;
   let output = '';
   let line = '';
   function drawPixel() {
-      if (Math.abs(cycle - x) <= 1) {
-          line += '#';
-      } else {
-          line += '.';
-      }
-      if (line.length >= 40) {
-          output += `${line}
-`;
-          line = '';
-          cycle = 0;
-      }
+    console.log(pixel, x);
+    if (Math.abs(pixel - x) <= 1) {
+      line += '#';
+    } else {
+      line += ' ';
+    }
+    if (line.length >= 40) {
+      output += line + '\n';
+      line = '';
+      // The last pixel of the nth line can be considered the -1th pixel of the n+1st line
+      pixel = -1;
+    }
   }
   for (const line of input.split('\n')) {
-      const addMatch = /addx (-?\d+)/.exec(line);
-      if (line === 'noop') {
-          cycle += 1;
-          drawPixel();
-      } else if (addMatch) {
-          cycle += 1;
-          drawPixel();
-          cycle += 1;
-          x += +addMatch[1];
-          drawPixel();
-      }
+    const addMatch = /addx (-?\d+)/.exec(line);
+    if (line === 'noop') {
+      drawPixel();
+      pixel += 1;
+    } else if (addMatch) {
+      drawPixel();
+      pixel += 1;
+      drawPixel();
+      pixel += 1;
+      x += +addMatch[1];
+    }
   }
   return output;
 }
